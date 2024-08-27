a7c45541756709265e66443b46f36e921fafe9b2
sshavn <57382710+sshavn@users.noreply.github.com>
2024-08-27 17:03:11 +0500

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,7 +1,7 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 my_index = 0
 my_len = len(my_list) - 1
-while my_index < my_len or my_list[my_index] > 0:
+while my_index <= my_len:
     if my_list[my_index] > 0:
         print(my_list[my_index])
         my_index = my_index + 1
