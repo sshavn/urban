5b076ae621a22a15ac93f4b759091cc9870f6cdf
sshavn <57382710+sshavn@users.noreply.github.com>
2024-08-27 16:54:29 +0500

    Create module_2_3.py

---
+++ module_2_3.py
@@ -0,0 +1,20 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+my_index = 0
+my_len = len(my_list)
+while my_index < my_len - 1 or my_list[my_index] > 0:
+    if my_list[my_index] > 0:
+        print(my_list[my_index])
+        my_index = my_index + 1
+        if my_index <= my_len - 1:
+            continue
+        else:
+            break
+    else:
+        if my_list[my_index] == 0:
+            my_index = my_index + 1
+            if my_index <= my_len - 1:
+                continue
+            else:
+                break
+        else:
+            break
